2c62b5dda768a7303297cf154041f710d0e04534
Chakshu Daithankar <chakshu.daithankar@blazeclan.com>
2021-05-22 14:18:35 +0530

    final commit

--- ecomreact/src/reduxapp/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "redux";
-
-import { addDeptReducer } from "./adddeptreducer";
-export const rootReducer = combineReducers({
-    departments: addDeptReducer // the modified state returned by the addDeptReducer will be stored in the 'departments' state property of the store
-});
